be02a89dadf05fc4f38b5faf18bcab3a3af66cf5
tanveerprottoy <tanveer.prottoy@gmail.com>
2025-02-23 00:07:12 +0600

    add mutex code

--- src/thread/mutex.c
+++ src/thread/mutex.c
@@ -2,3 +2,18 @@
 
 pthread_mutex_t mutex;
 
+int count;
+
+int read() {
+    return count;
+}
+
+void increment() {
+    // lock
+    pthread_mutex_lock(&mutex);
+
+    count++;
+
+    // unlock
+    pthread_mutex_unlock(&mutex);
+}
