ab0c84abb926d0bdcd6a9541c6703ffc6b86100d
tanvirprottoy <tanvir.prottoy@goldeninfotech.com.bd>
2025-02-25 17:43:57 +0600

    add multi thread code

--- src/main.c
+++ src/main.c
@@ -3,7 +3,63 @@
 #include "problems/leetcode.c"
 #include "thread/mutex.c"
 
-int main() {
+void *count_entry() {
+    increment();
+
+    printf("%d\n", read());
+
+    return NULL;
+}
+
+void multi_threading_count() {
+    pthread_t thread1, thread2;
+
+    // create thread 1
+    pthread_create(&thread1, NULL, count_entry, NULL);
+
+    // create thread 2
+    pthread_create(&thread2, NULL, count_entry, NULL);
+
+    // wait for thread 1 to finish
+    pthread_join(thread1, NULL);
+
+    // wait for thread 2 to finish
+    pthread_join(thread2, NULL);
+}
+
+void *thread_entry(void *arg) {
+    printf("Thread created: %p\n", arg);
+
+    char *message = (char *)arg;
+
+    // dereference the argument
+    printf("%s\n", *message);
+
+    return NULL;
+}
+
+void multi_threading_simple() {
+    pthread_t thread1, thread2;
+
+    char hello[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    char message0[] = "Hello, World! from thread 1";
+    char message1[] = "Hello, World! from thread 2";
+
+    // create thread 1
+    pthread_create(&thread1, NULL, thread_entry, &message0);
+
+    // create thread 2
+    pthread_create(&thread2, NULL, thread_entry, &message1);
+
+    // wait for thread 1 to finish
+    pthread_join(thread1, NULL);
+
+    // wait for thread 2 to finish
+    pthread_join(thread2, NULL);
+}
+
+void misc() {
     //     mem_alloc();
     /*  short a;
      int i;
@@ -26,10 +82,18 @@ int main() {
     int size = sizeof(nums) / sizeof(nums[0]);
 
     printf("%d\n", removeDuplicates(ptr, size)); */
+}
 
-    increment();
+void thread_safe_counter() {
+    /* increment();
 
-    printf("%d\n", read());
+    printf("%d\n", read()); */
+}
 
-    return 0;
+int main() {
+    multi_threading_simple();
+
+    // multi_threading_count();
+
+    return EXIT_SUCCESS;
 }
